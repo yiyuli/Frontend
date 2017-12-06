1aaa7ab3ff2c9564dbe63d3c56c6179b0cc0a296
yiyuli <yiyuli002@gmail.com>
2017-12-06 11:23:51 -0600

    use remote db

--- source/components/Home/Home.jsx
+++ source/components/Home/Home.jsx
@@ -23,7 +23,7 @@ class Home extends Component {
 
     componentWillMount() {
         this.setState({loading: true});
-        axios.get('http://localhost:5000/').then(function (response) {
+        axios.get('https://ejp-backend.herokuapp.com/https://ejp-backend.herokuapp.com/').then(function (response) {
             console.log(response);
             this.setState({
                 loading: false,
