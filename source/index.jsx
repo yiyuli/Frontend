270631e83608451e9345da84b5de12fe6f3b1e92
yiyuli <yiyuli002@gmail.com>
2017-12-11 11:05:58 -0600

    try fix deploy issue

--- source/index.jsx
+++ source/index.jsx
@@ -30,17 +30,6 @@ const BaseLayout = () => (
                     </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home
-                                    <span className="sr-only">(current)</span>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Services</a>
-                            </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Contact</a>
                             </li>
