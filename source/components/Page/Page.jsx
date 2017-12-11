0f0311c1dd74a0be7ab941ffdefe4ddc24d1a709
mliljenberg <mliljenberg@gmail.com>
2017-12-10 20:39:15 -0600

    comit before merge

---
+++ source/components/Page/Page.jsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react'
+import {Button} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+var axios = require('axios');
+
+class Page extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            data: '',
+        };
+        this.componentWillMount = this.componentWillMount.bind(this)
+    }
+    componentWillMount() {
+
+        axios.get('http://localhost:5000/program/1').then(function (response) {
+            console.log(response);
+            this.setState({
+                data: response.data
+            });
+        }.bind(this)).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    render() {
+
+        var name = this.state.data.name;
+        var state = this.state.data.state;
+        var desc = this.state.data.long_desc;
+        var website = this.state.data.website;
+        if(this.state.data =''){
+            return (<h1>No data</h1>);
+        }
+        let list = [];
+
+
+        if(this.state.data.for_credit == 'true'){
+            list.push(<li>Credit</li>);
+        }
+        if(this.state.data.two_year == true){
+            list.push(<li>Two Year Program</li>);
+        }
+        if(this.state.data.four_year == true){
+            list.push(<li>Four Year Program</li>);
+        }
+        if(this.state.data.arts_program === true){
+            console.log("hej");
+            list.push(<li>Arts Program</li>);
+        }
+        console.log(list);
+        return(
+        <div className="container full">
+            <div className="row">
+                <div className="col-md-10">
+                    <h1>{name} , {state}</h1>
+                    <a src={website}>{website}</a>
+                   <ul>
+                    {list.map((item) =>
+                    item
+                    )}
+                   </ul>
+                    <p>{desc}</p>
+                </div>
+                <aside className="float-right col-md-2">
+                    <div  />
+                </aside>
+            </div>
+        </div>
+    )
+    }
+}
+
+export default Page
--- source/index.jsx
+++ source/index.jsx
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 
 // Include your new Components here
 import Home from './components/Home/Home.jsx';
+import Page from './components/Page/Page.jsx';
 import './js/bootstrap.bundle.min';
 import './js/jquery.min'
 
@@ -17,8 +18,51 @@ require('./styles/content.css');
 require('./styles/shop-homepage.css');
 
 
+
+const BaseLayout = () => (
+    <div className="base">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Start Bootstrap</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarResponsive"
+                            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home
+                                    <span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        <div className="container-fluid">
+            <Route path="/" exact component={Home} />
+            <Route path="/page" component={Page} />
+        </div>
+        <footer>
+           Browser Example (c) 2017
+        </footer>
+    </div>
+)
 render(
-    <Home />,
+<Router>
+    <BaseLayout/>
+</Router>,
+
     // Define your router and replace <Home /> with it!
     document.getElementById('app')
 );
